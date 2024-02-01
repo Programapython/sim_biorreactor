5b363a0fe17ba415168b081cb415d4842c133774
Gustavo <perezgustavo2103@gmail.com>
2024-02-01 16:35:20 -0500

    cambios nuevos

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-function Iniciar_simulacion(){}
+const pala1 = document.querySelector('.pala1');
+const pala2 = document.querySelector('.pala2');
+const pala3 = document.querySelector('.pala3');
+function iniciar_simulacion(){
+    pala1.style.animation = "rotar 0.5s linear infinite";
+    pala2.style.animation = "rotar 0.5s linear infinite";
+    pala3.style.animation = "rotar 0.5s linear infinite";
+}
